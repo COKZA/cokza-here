6ab7336e4b9905349028563c0088959aaabacb46
COKZA <bodapower0@gmail.com>
2024-12-18 06:13:45 +0400

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,28 +1,2 @@
 import os
-import shutil
-import sys
-from pathlib import Path
-
-def add_to_startup(script_path=None):
-    # إذا لم يتم تحديد مسار السكربت، يتم استخدام ملف البرنامج الحالي
-    if script_path is None:
-        script_path = sys.argv[0]
-
-    # تحديد مسار Startup (ملف البرامج التي تعمل عند بدء التشغيل)
-    startup_folder = Path(os.getenv("APPDATA")) / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup"
-
-    # التحقق من وجود المجلد
-    if not startup_folder.exists():
-        return
-
-    # نسخ ملف السكربت إلى مجلد Startup
-    script_name = Path(script_path).name
-    destination = startup_folder / script_name
-
-    try:
-        shutil.copy(script_path, destination)
-    except:
-        pass
-
-# استدعاء الوظيفة
-add_to_startup()
+print("asdzxcvqwe-)_+^%&$#@%@!#~!#~asd52x0r7405")
